5ee2b82cee5a2a27d82e79b2ec87b48d9985cd64
Laz <LazDisco@users.noreply.github.com>
2019-03-30 21:01:26 +0000

    Update Defaults.cs

--- Properties/Defaults.cs
+++ Properties/Defaults.cs
@@ -7,7 +7,7 @@ namespace DSLauncherV2.Properties
     internal sealed class Defaults : ApplicationSettingsBase
     {
         public static Defaults Settings { get; } = (Defaults) Synchronized(new Defaults());
-        [DefaultSettingValue("http://patch.discoverygc.net/patchlist.xml")]
+        [DefaultSettingValue("http://patch.discoverygc.net/")]
         [ApplicationScopedSetting]
         [DebuggerNonUserCode]
         public string KittyURL => (string) this[nameof(KittyURL)];
